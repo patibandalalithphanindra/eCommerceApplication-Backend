0588cc9b64a1762b5a6756ca9042c4ca933b6f8a
patibandalalithphanindra <71619611+patibandalalithphanindra@users.noreply.github.com>
2023-08-21 22:11:42 +0530

    Delete eCommerceApplication/src/main/java/com/lalith/backendproject/eCommerceApplication/Repository directory

--- eCommerceApplication/src/main/java/com/lalith/backendproject/eCommerceApplication/Repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lalith.backendproject.eCommerceApplication.Repository;
-
-import com.lalith.backendproject.eCommerceApplication.Model.Inventory;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InventoryRepository extends MongoRepository<Inventory,String> {
- }
